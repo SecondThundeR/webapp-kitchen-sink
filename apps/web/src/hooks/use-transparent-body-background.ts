c94c6f834be1f0f7613efe8383275adb3e7bbadd
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2026-02-20 02:41:56 +0300

    Revert "refactor(web): update useTransparentBackground hook"
    This reverts commit b3e6471136282b41242b29d8a0fb8774fa4f12d1.

--- apps/web/src/hooks/use-transparent-body-background.ts
+++ apps/web/src/hooks/use-transparent-body-background.ts
@@ -1,15 +1,13 @@
 import { useEffect } from "react";
 
-export const useTransparentBackground = (enabled = true) => {
+export const useTransparentBodyBackground = (enabled = true) => {
   useEffect(() => {
     if (!enabled) return;
 
-    document.documentElement.classList.add("bg-transparent");
     document.body.classList.add("bg-transparent");
 
     return () => {
-      document.documentElement.classList.remove("bg-transparent");
       document.body.classList.remove("bg-transparent");
     };
-  }, [enabled]);
+  });
 };
--- apps/web/src/pages/theme-params/theme-params.tsx
+++ apps/web/src/pages/theme-params/theme-params.tsx
@@ -1,4 +1,4 @@
-import { useTransparentBackground } from "@/hooks/use-transparent-background";
+import { useTransparentBodyBackground } from "@/hooks/use-transparent-body-background";
 import { isVersionAtLeastFilter } from "@/utils/array";
 import { DynamicThemeCard } from "../../components/dynamic-theme-card";
 import { ThemeCard } from "../../components/theme-card";
@@ -13,7 +13,7 @@ export const ThemeParamsPage = () => {
   const themeParams = useThemeParams();
 
   useBottomButton();
-  useTransparentBackground();
+  useTransparentBodyBackground();
 
   const filteredCustomizableThemeFields = CUSTOMIZABLE_THEME_FIELDS.filter(
     isVersionAtLeastFilter,
