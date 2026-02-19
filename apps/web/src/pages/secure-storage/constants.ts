ae49b2a97e19fb30109ab290c8b0d8843479c84d
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2026-02-19 18:51:35 +0300

    fix(web): remove support for tdesktop in Secure Storage Playground

---
+++ apps/web/src/pages/secure-storage/constants.ts
@@ -0,0 +1,1 @@
+export const UNSUPPORTED_PLATFORMS = ["tdesktop"];
--- apps/web/src/pages/secure-storage/index.tsx
+++ apps/web/src/pages/secure-storage/index.tsx
@@ -1,11 +1,39 @@
+import { FrownIcon } from "lucide-react";
+import {
+  Empty,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty";
 import { withWebAppVersion } from "@/hocs/web-app-version";
+import { WebApp } from "@/lib/web-app";
 import { Clear } from "./components/clear";
 import { GetItem } from "./components/get-item";
 import { RemoveItem } from "./components/remove-item";
 import { RestoreItem } from "./components/restore-item";
 import { SetItem } from "./components/set-item";
+import { UNSUPPORTED_PLATFORMS } from "./constants";
 
 const SecureStorageComponent = () => {
+  if (UNSUPPORTED_PLATFORMS.includes(WebApp.platform)) {
+    return (
+      <Empty className="flex-1">
+        <EmptyHeader>
+          <EmptyMedia variant="icon">
+            <FrownIcon />
+          </EmptyMedia>
+          <EmptyTitle>Not supported</EmptyTitle>
+          <EmptyDescription>
+            This client doesn't support SecureStorage
+            <br />
+            Try to use other Telegram client that supports it
+          </EmptyDescription>
+        </EmptyHeader>
+      </Empty>
+    );
+  }
+
   return (
     <div className="flex flex-col gap-2">
       <h1 className="text-2xl font-semibold tracking-tight">
