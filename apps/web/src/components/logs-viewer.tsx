bd2170df06ec53efc120fd3b7a3d392e60e3e3c2
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2026-02-22 22:06:39 +0300

    feat(web): update LogsViewer styles for light theme

--- apps/web/src/components/logs-viewer.tsx
+++ apps/web/src/components/logs-viewer.tsx
@@ -10,14 +10,14 @@ export const LogsViewer = ({ logs, compact = false }: LogsViewerProps) => {
   return (
     <div
       className={cn(
-        "h-32 overflow-y-auto bg-black text-green-400 p-2 text-xs font-mono rounded-md",
+        "h-32 overflow-y-auto border dark:border-0 dark:bg-black text-green-700 dark:text-green-400 p-2 text-xs font-mono rounded-md",
         {
           "h-20": compact,
         },
       )}
     >
       {logs.length === 0 && (
-        <span className="opacity-50">{"// Logs will appear here..."}</span>
+        <span className="opacity-60 dark:opacity-50">{"// Logs will appear here..."}</span>
       )}
       {logs.map(({ timestamp, message }) => (
         <div key={timestamp}>
