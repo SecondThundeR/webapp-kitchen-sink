7dcc7caa31191c465f1b97e1ec7e7f31bc7b6424
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2026-03-15 13:58:42 +0300

    chore(*): bump dependencies

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,8 @@
 import { execSync } from "node:child_process";
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
 import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig, loadEnv } from "vite";
 import svgr from "vite-plugin-svgr";
@@ -23,11 +24,8 @@ export default defineConfig(({ mode }) => {
   }
 
   const plugins = [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
     svgr(),
   ];
